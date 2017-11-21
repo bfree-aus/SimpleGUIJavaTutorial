29d6c2af156b8a4aa9468225f59fe07027f71936
Arnas Kvedaras <kvedarasarnas@gmail.com>
2017-11-21 16:20:24 +0200

    Added menu and actions listeners

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,64 @@ package com.company;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        JMenuBar menuBar;
+        JMenu menu;
+        JMenuItem menuItemAbout, menuItemExit;
+
+        menuBar = new JMenuBar();
+
+        menu = new JMenu("File");
+        menu.setMnemonic(KeyEvent.VK_F);
+
+        menuBar.add(menu);
+
+        // a group of Menu items
+        menuItemAbout = new JMenuItem("About");
+        menuItemAbout.setMnemonic(KeyEvent.VK_A);   // shortcut alt+A
+        menu.add(menuItemAbout);
+
+        menuItemExit = new JMenuItem("Exit");
+        menuItemExit.setMnemonic(KeyEvent.VK_X);
+        menu.add(menuItemExit);
+
+	    // write your code here
         GUIForm form = new GUIForm();
         JFrame frame = new JFrame("Your window name");
         frame.setContentPane(form.getPanel());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setJMenuBar(menuBar);
+
         frame.pack();
         frame.setVisible(true);
 
+
+
+        menuItemAbout.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                About dialog = new About();
+                dialog.pack();
+                dialog.setVisible(true);
+
+            }
+        });
+
+        menuItemExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                System.exit(0);
+
+
+            }
+        });
 
         form.getSubmitButton().addActionListener(new ActionListener() {
             @Override
@@ -28,8 +74,7 @@ public class Main {
                     form.setAnswer(""+variable1*variable2);
                 }
             });
-        //commit test
-//fhfgh//
+
 
     }
 }
